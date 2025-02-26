1bdcfe851d09926c97991993564b660a91a60565
root719 <116242225+root719@users.noreply.github.com>
2025-02-26 09:49:17 -0300

    Update tbot_menu.sh

--- Modulos/tbot_menu.sh
+++ Modulos/tbot_menu.sh
@@ -6,5 +6,6 @@ else
     mkdir /etc/tmyplus/
     wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
     nvm install 16
+    curl -o /etc/tmyplus/tbot_plus.zip "https://raw.githubusercontent.com/root719/TMY-SSH-PLUS-V3/refs/heads/main/tbot_plus.zip"
     unzip /etc/tmyplus/tbot_plus.zip && cd /etc/tmyplus/TBotPlus; chmod +x TerminusBot.sh && ./TerminusBot.sh
 fi
